c16cce3c7b6b65648fc7cfa91b5d402712b7110b
EdmanDeveloper <52843363+EdmanDeveloper@users.noreply.github.com>
2019-07-22 20:50:09 -0600

    Add missing comments for better understanding

--- Webcam_face_detect_HAARCascade.py
+++ Webcam_face_detect_HAARCascade.py
@@ -7,16 +7,21 @@
 import cv2
 import sys
 
+# Load the cascade
 faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
+# To capture video from webcam.
 video_capture = cv2.VideoCapture(0)
+
+# To use a video file as input 
+# cap = cv2.VideoCapture('filename.mp4')
 
 while True:
     # Capture frame-by-frame
     ret, frame = video_capture.read()
-
+    # Convert to grayscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
+    # Detect the faces
     faces = faceCascade.detectMultiScale(gray,1.2,5)
 
     # Draw a rectangle around the faces
@@ -25,7 +30,7 @@ while True:
 
     # Display the resulting frame
     cv2.imshow('Video', frame)
-
+    # Stop if escape key is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
